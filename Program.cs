f5c1cb4070d39b943652c887e94759f751c71bcf
Alex <pigogav53@gmail.com>
2022-12-24 21:15:12 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int[] myArray = Auto();
+int count = 0;
+
+for (int i = 0; i < myArray.Length; i++)
+    {
+
+        if (myArray[i] > 0)
+        {
+            count ++;
+        }
+    }
+int[] Auto()
+{
+    int[] myArray;
+    myArray = new int[5];
+    for (int i = 0; i < myArray.Length; i++)
+    {
+
+        System.Console.WriteLine($"Введите значение элемента массива индекса {i}");
+        myArray[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    return myArray;
+}
+System.Console.WriteLine("Наш массив: [" + string.Join(", ", myArray) + "]");
+System.Console.WriteLine($"Пользователь ввел {count} положительных чисел");
+
